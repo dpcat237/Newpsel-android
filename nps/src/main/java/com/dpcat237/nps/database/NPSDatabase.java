d4e8cb51fa556f861dcb1feccaafb3836dfbdf7f
Denys Pasishnyi <dpcat237@gmail.com>
2014-05-09 18:11:51 +0200

    Add lastUpdate value to set to 0 when database is updated

--- nps/src/main/java/com/dpcat237/nps/database/NPSDatabase.java
+++ nps/src/main/java/com/dpcat237/nps/database/NPSDatabase.java
@@ -4,13 +4,17 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.dpcat237.nps.helper.GenericHelper;
+
 public class NPSDatabase extends SQLiteOpenHelper {
 
+    private Context mContext;
 	private static final String DATABASE_NAME = "nps.db";
 	private static final int DATABASE_VERSION = 4;
 	
 	public NPSDatabase(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        mContext = context;
 	}
 	
 	@Override
@@ -32,11 +36,14 @@ public class NPSDatabase extends SQLiteOpenHelper {
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Integer lastUpdate = 0;
 		FeedTable.onUpgrade(db, oldVersion, newVersion);
 		ItemTable.onUpgrade(db, oldVersion, newVersion);
 		LabelTable.onUpgrade(db, oldVersion, newVersion);
 		LabelItemTable.onUpgrade(db, oldVersion, newVersion);
 		SharedTable.onUpgrade(db, oldVersion, newVersion);
+        GenericHelper.setLastFeedsUpdate(mContext, lastUpdate);
+        GenericHelper.setLastLabelsUpdate(mContext, lastUpdate);
 	}
 
 }
