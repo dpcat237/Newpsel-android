6b7dd9589f6fc28fbb9e98ff70b8b8e9a5439a8b
Denys Pasishnyi <dpcat237@gmail.com>
2014-06-20 11:08:37 +0200

    Add notification helper to send simple notifications

---
+++ nps/src/main/java/com/dpcat237/nps/helper/NotificationHelper.java
@@ -0,0 +1,31 @@
+package com.dpcat237.nps.helper;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import com.dpcat237.nps.R;
+import com.dpcat237.nps.activity.MainActivity;
+
+public class NotificationHelper {
+    public static final int NOTIFICATION_ID = 1;
+
+    public static void showSimpleNotification(Context context, String msg) {
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle(context.getString(R.string.nt_download))
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(msg))
+                        .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+}
