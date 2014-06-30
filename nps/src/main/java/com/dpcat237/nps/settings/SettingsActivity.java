667a803eb8a541f763fd64ba92b536001558a613
Denys Pasishnyi <dpcat237@gmail.com>
2014-06-30 13:40:59 +0200

    Add dictation label select in settings

--- nps/src/main/java/com/dpcat237/nps/settings/SettingsActivity.java
+++ nps/src/main/java/com/dpcat237/nps/settings/SettingsActivity.java
@@ -1,11 +1,11 @@
-package com.dpcat237.nps.preference;
+package com.dpcat237.nps.settings;
 
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
 import com.dpcat237.nps.R;
 
-public class PrefsFragment extends PreferenceFragment {
+public class SettingsActivity extends PreferenceFragment {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
