cc3b7b2c329ead22bc8a72e0b4c2134fbe7573c7
Denys Pasishnyi <dpcat237@gmail.com>
2014-08-04 19:48:12 +0200

    Widget: play dictations when player isn't active

--- nps/src/main/java/com/dpcat237/nps/ui/widget/PlayerWidgetProvider.java
+++ nps/src/main/java/com/dpcat237/nps/ui/widget/PlayerWidgetProvider.java
@@ -13,6 +13,8 @@ import com.dpcat237.nps.behavior.factory.SongsFactory;
 import com.dpcat237.nps.behavior.service.PlayerService;
 import com.dpcat237.nps.behavior.service.valueObject.PlayerServiceStatus;
 import com.dpcat237.nps.constant.PlayerConstants;
+import com.dpcat237.nps.constant.SongConstants;
+import com.dpcat237.nps.database.repository.DictateItemRepository;
 import com.dpcat237.nps.model.Song;
 import com.dpcat237.nps.ui.activity.MainActivity;
 import com.dpcat237.nps.ui.dialog.PlayerLabelsDialog;
@@ -20,23 +22,36 @@ import com.dpcat237.nps.ui.dialog.PlayerLabelsDialog;
 
 public class PlayerWidgetProvider extends AppWidgetProvider {
     private static final String TAG = "NPS:PlayerWidgetProvider";
+    private Context mContext;
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         if (appWidgetIds.length == 0) {
             return;
         }
+        mContext = context;
 
         PlayerServiceStatus playerStatus = PlayerServiceStatus.getInstance();
         Song song = playerStatus.getCurrentSong();
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_player);
+
+        //check if are dictations to play
+        DictateItemRepository dictateRepo = new DictateItemRepository(context);
+        dictateRepo.open();
+        Integer unreadCount = dictateRepo.countUnreadGrabberItems();
+        dictateRepo.close();
+
         for (int widgetId : appWidgetIds) {
             updateSongDetails(playerStatus, song, views);
             if (!playerStatus.hasActiveSong()) {
                 views.setOnClickPendingIntent(R.id.buttonDetails, PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), 0));
                 PendingIntent pendingIntent = PendingIntent.getService(context, 0, new Intent(), 0);
                 views.setOnClickPendingIntent(R.id.buttonBackward, pendingIntent);
-                views.setOnClickPendingIntent(R.id.buttonPausePlay, pendingIntent);
+                if (unreadCount > 0) {
+                    views.setOnClickPendingIntent(R.id.buttonPausePlay, getPlayIntent());
+                } else {
+                    views.setOnClickPendingIntent(R.id.buttonPausePlay, pendingIntent);
+                }
                 views.setOnClickPendingIntent(R.id.buttonForward, pendingIntent);
                 views.setOnClickPendingIntent(R.id.buttonAddLabel, pendingIntent);
                 views.setOnClickPendingIntent(R.id.songTitle, pendingIntent);
@@ -46,9 +61,9 @@ public class PlayerWidgetProvider extends AppWidgetProvider {
             }
 
             // set up pending intents
-            setClickIntent(context, views, R.id.buttonBackward, PlayerConstants.INTENT_DATA_BACKWARD, PlayerConstants.PLAYER_COMMAND_SKIPBACK);
-            setClickIntent(context, views, R.id.buttonPausePlay, PlayerConstants.INTENT_DATA_PLAYPAUSE, PlayerConstants.PLAYER_COMMAND_PLAYPAUSE);
-            setClickIntent(context, views, R.id.buttonForward, PlayerConstants.INTENT_DATA_FORWARD, PlayerConstants.PLAYER_COMMAND_SKIPFORWARD);
+            setClickIntent(views, R.id.buttonBackward, PlayerConstants.INTENT_DATA_BACKWARD, PlayerConstants.PLAYER_COMMAND_SKIPBACK);
+            setClickIntent(views, R.id.buttonPausePlay, PlayerConstants.INTENT_DATA_PLAYPAUSE, PlayerConstants.PLAYER_COMMAND_PLAYPAUSE);
+            setClickIntent(views, R.id.buttonForward, PlayerConstants.INTENT_DATA_FORWARD, PlayerConstants.PLAYER_COMMAND_SKIPFORWARD);
 
             // show item view
             Intent detailsIntent = SongsFactory.getActivityIntent(context, song.getType());
@@ -91,11 +106,20 @@ public class PlayerWidgetProvider extends AppWidgetProvider {
         return text.substring(0, 35)+"...";
     }
 
-    private static void setClickIntent(Context context, RemoteViews views, int resourceId, String data, int command) {
-        Intent intent = new Intent(context, PlayerService.class);
+    private void setClickIntent(RemoteViews views, int resourceId, String data, int command) {
+        Intent intent = new Intent(mContext, PlayerService.class);
         intent.setData(Uri.parse(data));
         intent.putExtra(PlayerConstants.EXTRA_PLAYER_COMMAND, command);
-        PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+        PendingIntent pendingIntent = PendingIntent.getService(mContext, 0, intent, 0);
         views.setOnClickPendingIntent(resourceId, pendingIntent);
+    }
+
+    private PendingIntent getPlayIntent() {
+        Intent intent = new Intent(mContext, PlayerService.class);
+        intent.putExtra(PlayerConstants.EXTRA_PLAYER_COMMAND, PlayerConstants.PLAYER_COMMAND_PLAYPAUSE_LIST);
+        intent.putExtra(PlayerConstants.EXTRA_PLAYER_TYPE, SongConstants.GRABBER_TYPE_DICTATE_ITEM);
+        intent.putExtra(PlayerConstants.EXTRA_PLAYER_COMMAND_ARG, 0);
+
+        return PendingIntent.getService(mContext, 0, intent, 0);
     }
 }
