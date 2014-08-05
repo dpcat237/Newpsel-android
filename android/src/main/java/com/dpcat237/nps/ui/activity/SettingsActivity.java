a5520275abd51a0b2099ad4182daa9b6e55aaafc
Denys Pasishnyi <dpcat237@gmail.com>
2014-08-05 20:34:50 +0200

    Add settings for later items

--- android/src/main/java/com/dpcat237/nps/ui/activity/SettingsActivity.java
+++ android/src/main/java/com/dpcat237/nps/ui/activity/SettingsActivity.java
@@ -1,10 +1,15 @@
 package com.dpcat237.nps.ui.activity;
 
 import android.os.Bundle;
+import android.preference.MultiSelectListPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 
 import com.dpcat237.nps.R;
+import com.dpcat237.nps.database.repository.LabelRepository;
+import com.dpcat237.nps.model.Label;
+
+import java.util.ArrayList;
 
 public class SettingsActivity extends PreferenceActivity {
     private static final String TAG = "NPS:SettingsActivity";
@@ -23,6 +28,30 @@ public class SettingsActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
+
+            MultiSelectListPreference labelsList = (MultiSelectListPreference) findPreference("pref_later_items_labels");
+            setLaterItemsLabels(labelsList);
+        }
+
+        private void setLaterItemsLabels(MultiSelectListPreference labelsList) {
+            LabelRepository labelRepo = new LabelRepository(getActivity());
+            labelRepo.open();
+            ArrayList<Label> labels = labelRepo.getAllLabels();
+            labelRepo.close();
+
+            CharSequence[] entryValues = new CharSequence[labels.size()];
+            CharSequence[] entries = new CharSequence[labels.size()];
+            Integer count = 0;
+            Integer getId;
+            for (Label label : labels) {
+                getId = label.getId();
+                entryValues[count] = getId.toString();
+                entries[count] = label.getName();
+                count++;
+            }
+
+            labelsList.setEntryValues(entryValues);
+            labelsList.setEntries(entries);
         }
     }
 }
