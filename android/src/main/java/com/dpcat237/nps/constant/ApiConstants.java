55dce136433a0b61409965339ed2fbb91ff7ee5d
Denys Pasishnyi <dpcat237@gmail.com>
2014-10-12 16:24:47 +0200

    Adapt requests for https

--- android/src/main/java/com/dpcat237/nps/constant/ApiConstants.java
+++ android/src/main/java/com/dpcat237/nps/constant/ApiConstants.java
@@ -4,7 +4,7 @@ public class ApiConstants {
     public static final String TYPE_SYNC_DICTATE_ITEMS = "sync_dictate_items";
 
     /* URLs */
-    public static final String API_URL = "http://www.newpsel.com/api/";
+    public static final String API_URL = "https://www.newpsel.com/api/";
     public static final String URL_ADD_FEED = API_URL+"add_feed/";
     public static final String URL_ADD_GCM_ID = API_URL+"gcm/add_id/";
     public static final String URL_GET_FEEDS = API_URL+"sync_feeds/";
