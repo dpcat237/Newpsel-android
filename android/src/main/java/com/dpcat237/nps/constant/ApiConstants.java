8733b76a1941b1a470b10594cb349374abd7d998
Denys Pasishnyi <dpcat237@gmail.com>
2016-06-10 08:18:38 +0200

    Update API urls

--- android/src/main/java/com/dpcat237/nps/constant/ApiConstants.java
+++ android/src/main/java/com/dpcat237/nps/constant/ApiConstants.java
@@ -5,18 +5,18 @@ public class ApiConstants {
 
     /* URLs */
     public static final String API_URL = "https://www.newpsel.com/api/";
-    public static final String URL_ADD_FEED = API_URL+"add_feed/";
-    public static final String URL_ADD_GCM_ID = API_URL+"gcm/add_id/";
-    public static final String URL_GET_FEEDS = API_URL+"sync_feeds/";
-    public static final String URL_RECOVERY_PASSWORD = API_URL+"password_recovery/";
-    public static final String URL_SIGN_IN = API_URL+"login/";
-    public static final String URL_SIGN_UP = API_URL+"sign_up/";
-    public static final String URL_SYNC_DICTATE_ITEMS = API_URL+"sync_dictate_items/";
-    public static final String URL_SYNC_ITEMS_UNREAD = API_URL+"sync_unread/";
-    public static final String URL_SYNC_LABELS = API_URL+"sync_labels/";
-    public static final String URL_SYNC_LABEL_ITEMS = API_URL+"sync_later/";
-    public static final String URL_SYNC_LATER_ITEMS = API_URL+"sync_later_items/";
-    public static final String URL_SYNC_SHARED_ITEMS = API_URL+"sync_shared/";
+    public static final String URL_ADD_FEED = API_URL+"add_feed";
+    public static final String URL_ADD_GCM_ID = API_URL+"gcm/add_id";
+    public static final String URL_GET_FEEDS = API_URL+"sync_feeds";
+    public static final String URL_RECOVERY_PASSWORD = API_URL+"password_recovery";
+    public static final String URL_SIGN_IN = API_URL+"login";
+    public static final String URL_SIGN_UP = API_URL+"sign_up";
+    public static final String URL_SYNC_DICTATE_ITEMS = API_URL+"sync_dictate_items";
+    public static final String URL_SYNC_ITEMS_UNREAD = API_URL+"sync_unread";
+    public static final String URL_SYNC_LABELS = API_URL+"sync_labels";
+    public static final String URL_SYNC_LABEL_ITEMS = API_URL+"sync_later";
+    public static final String URL_SYNC_LATER_ITEMS = API_URL+"sync_later_items";
+    public static final String URL_SYNC_SHARED_ITEMS = API_URL+"sync_shared";
 
     /* ERRORS */
     public static final int ERROR_LOGIN_DATA = 301;
